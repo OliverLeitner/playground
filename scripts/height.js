0000c2632063f989d633098f4f34cdaea7d284f5
Oliver Leitner <never@neverslair-blog.net>
2019-10-06 00:49:34 +0200

    offsetHeight ff hack

--- scripts/height.js
+++ scripts/height.js
@@ -4,9 +4,16 @@
  */
 var content = document.getElementById("maincontent");
 var scHeight = content.scrollHeight;
-var oHeight = content.offsetHeight;
+var height = scHeight - content.clientHeight;
+
+// firefox does stuff a little bit different
+var browser = navigator.userAgent;
+if (!browser.includes("Chrome")) {
+    console.log("we are firefox");
+    height = height + 300;
+}
+
 // scrollheight - offsetheight
-var height = scHeight - oHeight;
 var columns = document.getElementsByClassName('flex-item');
 for (let item of columns) {
     item.setAttribute('style', 'padding-bottom:' + height + 'px;margin-bottom:-' + height + 'px;');
